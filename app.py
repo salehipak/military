9da2dd6bf36b924815401fa39c971d27e102b6cc
salehipak <153202036+salehipak@users.noreply.github.com>
2024-08-09 13:11:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def jaccard_similarity(set1, set2):
     intersection = len(set1.intersection(set2))
     union = len(set1.union(set2))
     try:
-        returnintersection / union
+        return intersection / union
     except ZeroDivisionError:
         return 0
 
